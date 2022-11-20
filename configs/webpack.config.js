00f3a894919f320d7f7020677ead0286e4ceccc9
Oleg Soshko <oleg.91.03@gmail.com>
2022-11-21 00:22:14 +0300

    настройка проекта

---
+++ configs/webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
+  devServer: {
+    port: 3030,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: {
+                mode: 'local',
+                localIdentName: "[name]__[local]--[hash:base64:5]",
+              }
+            },
+          },
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: "url-loader",
+        options: { limit: false },
+      },
+    ],
+  },
+};
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import styles from './main.module.scss';
+
+export const Main = () => {
+  return (
+    <div className={styles.content}>
+      Main
+    </div>
+  )
+}
